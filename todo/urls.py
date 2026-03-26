45c22f2526d7017daa9817a4e61232627b91a428
unknown <balajibala5397@gmail.com>
2026-03-26 13:45:04 +0530

    Modified ToDo Application

--- todo/urls.py
+++ todo/urls.py
@@ -6,12 +6,12 @@ urlpatterns = [
     path('addtask', views.addtask, name='addtask'),
 
     #mark as done
-    path('mark_as_done/<int:pk>/', views.mark_as_done, name ='mark_as_done'),
+    path('mark_as_done/<int:task_id>/', views.mark_as_done, name ='mark_as_done'),
     #mark as undone
-    path('mark_as_undone/<int:pk>/', views.mark_as_undone, name='mark_as_undone'),
+    path('mark_as_undone/<int:task_id>/', views.mark_as_undone, name='mark_as_undone'),
 
     #edit task
-    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
+    path('edit_task/<int:task_id>/', views.edit_task, name='edit_task'),
 
     #delete task
-    path('delete_task/<int:pk>/', views.delete_task, name='delete_task'),]
+    path('delete_task/<int:task_id>/', views.delete_task, name='delete_task'),]
--- todo/views.py
+++ todo/views.py
@@ -5,35 +5,35 @@ from todo.models import Task
 
 # Create your views here.
 def addtask(request):
-    t = request.POST.get('task')
+    t = request.POST.get('data')
     Task.objects.create(task=t)
     return redirect('homepg')
 
-def mark_as_done(request, pk):
-    task = Task.objects.get(pk=pk)
-    task.is_completed = True
-    task.save()
+def mark_as_done(request, task_id):
+    task_obj = Task.objects.get(pk=task_id)
+    task_obj.is_completed = True
+    task_obj.save()
     return redirect('homepg')
 
-def mark_as_undone(request,pk):
-    task = Task.objects.get(pk=pk)
-    task.is_completed = False
-    task.save()
+def mark_as_undone(request, task_id):
+    task_obj = Task.objects.get(pk=task_id)
+    task_obj.is_completed = False
+    task_obj.save()
     return redirect('homepg')
 
-def edit_task(request, pk):
-    task = Task.objects.get(pk=pk)
+def edit_task(request, task_id):
+    task_obj = Task.objects.get(pk=task_id)
     if request.method == 'POST':
         new_task = request.POST.get('task')
-        task.task = new_task
-        task.save()
+        task_obj.task = new_task
+        task_obj.save()
         return redirect('homepg')
     context = {
-        'task': task
+        'task': task_obj
     }
     return render(request, 'edit_task.html', context)
 
-def delete_task(request, pk):
-    task = Task.objects.get(pk=pk)
-    task.delete()   
+def delete_task(request, task_id):
+    task_obj = Task.objects.get(pk=task_id)
+    task_obj.delete()   
     return redirect('homepg')
